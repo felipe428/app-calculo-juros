bf319ff2bb2ef82f687131659439f6164141e275
felipe428 <felipe.silva428@fatec.sp.gov.br>
2021-08-28 00:01:00 -0300

    não sei mais

--- src/app/juros-simples/juros-simples.component.ts
+++ src/app/juros-simples/juros-simples.component.ts
@@ -6,21 +6,23 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./juros-simples.component.css']
 })
 export class JurosSimplesComponent implements OnInit {
-  @Input() v: number;
-  @Input() n: number;
-  @Input() j: number;
+  @Input() valor: number;
+  @Input() parcelas: number;
+  @Input() juros: number;
 
   constructor() {}
 
   ngOnInit() {}
 
   calculo() {
-    let vf: number;
-    let x: number;
-    for (let i = 1; i <= this.n; i++) {
-      vf = this.v * (1 + (this.j / 100) * this.n);
-      x = vf - this.v;
+    let list: number[] = [];
+    let valorfuturo: number;
+    let total: number;
+    for (let i = 1; i <= this.parcelas; i++) {
+      valorfuturo = this.valor * (1 + (this.juros / 100) * this.parcelas);
+      total = valorfuturo - this.valor;
+      list.push(total);
     }
-    return x;
+    return list;
   }
 }
